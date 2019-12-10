42025b7f3a68889af42cdc940cb5da863885abbc
Nate V <navogel@gmail.com>
2019-12-10 10:04:35 -0600

    first commit

---
+++ StudentExercisesContinued/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentExercisesContinued
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
