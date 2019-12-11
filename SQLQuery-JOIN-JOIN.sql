5822eb9c270bf8129b1bf8556f4b4c38d9f5b797
Nate V <navogel@gmail.com>
2019-12-11 11:27:19 -0600

    added new join join sql query with GROUP BY

---
+++ SQLQuery-JOIN-JOIN.sql
@@ -0,0 +1,10 @@
+﻿SELECT s.FirstName, s.LastName, e.Name, e.Language FROM StudentExercise se
+JOIN Student s on s.Id = se.StudentId
+JOIN Exercise e on e.Id = se.ExerciseId;
+
+--groupby
+
+SELECT s.CohortId, c.Name, COUNT(s.CohortId) StudentCount 
+FROM Student s
+JOIN Cohort c ON s.CohortId = c.Id
+GROUP BY c.Name, CohortId;
